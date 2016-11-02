49da9b49213af31f6b1d85f3dfbf4ec18978dab7
Hadouweb <kengo14@hotmail.fr>
2016-11-02 11:50:29 +0100

    Comprehension of subject

--- srcs/main.c
+++ srcs/main.c
@@ -25,33 +25,11 @@ int 	m_read_cmd(char **cmd)
 int		main(int ac, char **av, char **envp)
 {
 	//pid_t	pid;
+	//pid_t	pid2;
 
 
 	if (av[1] && ac && envp)
 		;
-	//pid = fork();
-	/*if (pid == 0)
-	{
-		printf("In Child\n");
-		if (ac > 1)
-		{
-			char buff[256];
-			char *path = "/bin/env";
-			printf("%d\n", chdir("/Users/nle-bret/projects/"));
-			char *apath = getcwd(buff, 256);
-			printf("%s\n", apath);
-			printf("%d\n", access(path, R_OK));
-			printf("%d\n", access(path, W_OK));
-			printf("%d\n", access(path, X_OK));
-			printf("%d\n", access(path, F_OK));
-			if (access(path, X_OK) != -1)
-				execve(path, av, NULL);
-		}
-	}
-	if (pid > 0)
-	{*/
-
-
 	char *cmd;
 
 	while (1)
@@ -61,12 +39,5 @@ int		main(int ac, char **av, char **envp)
 		m_read_cmd(&cmd);
 		ft_strdel(&cmd);
 	}
-		//ft_strdel(&line);
-
-
-		//for (char **env = envp; *env; ++env)
-		//	printf("%s\n", *env);
-		//wait(&pid);
-	//}
 	return 0;
 }
