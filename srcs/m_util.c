bdacda65824319da61d5f47af427dbca66f9d9c2
hadouweb <kengo14@hotmail.fr>
2016-11-03 20:32:04 +0100

    Pqrsing escaped character

--- srcs/m_util.c
+++ srcs/m_util.c
@@ -4,36 +4,76 @@ int 	m_word_size_cmd(char *cmd)
 {
 	int 	i;
 	int 	token;
+	int 	token2;
 
 	token = 0;
+	token2 = 0;
 	i = 0;
 	while (cmd[i])
 	{
-		if (token == 2)
+		if (token == 2 || token2 == 2)
 			return (i);
-		if ((cmd[i] == ' ' || cmd[i] == '\t') && token == 0)
+		if ((cmd[i] == ' ' || cmd[i] == '\t') && token == 0 && token2 == 0)
 			return (i);
 		if (cmd[i] == '"' && !(cmd[i - 1] && cmd[i - 1] == '\\'))
 			token++;
+		if (cmd[i] == '\'' && !(cmd[i - 1] && cmd[i - 1] == '\\'))
+			token2++;
 		i++;
 	}
 	return i;
 }
 
-void	m_set_escaped_character(char *new_str, char *str, int *i, int max)
+int		m_set_escaped_character(char *new_str, char *str, int mode)
 {
-	printf("%s\n", str);
-	if (*i < max && str[1])
+	int	i;
+	//printf("%s\n", str);
+	i = 0;
+	if (str[1])
 	{
-		printf("%c %c\n", str[0], str[1]);
+		//printf("%c %c\n", str[0], str[1]);
 		if (str[1] == '\\') {
 			new_str[0] = '\\';
-			(*i)++;
-		} else if (str[1] == '"') {
+			i += 2;
+		}
+		else if (str[1] == '"' && mode == 1) {
 			new_str[0] = '"';
-			(*i)++;
+			i++;
+		}
+		else if (str[1] == '\'' && mode == 2) {
+			new_str[0] = '\'';
+			i++;
+		}
+		else if (str[1] == 'n') {
+			new_str[0] = '\n';
+			i += 2;
+		}
+		else if (str[1] == 'v') {
+			new_str[0] = '\v';
+			i += 2;
+		}
+		else if (str[1] == 'a') {
+			new_str[0] = '\a';
+			i += 2;
+		}
+		else if (str[1] == 'b') {
+			new_str[0] = '\b';
+			i += 2;
+		}
+		else if (str[1] == 'r') {
+			new_str[0] = '\r';
+			i += 2;
+		}
+		else if (str[1] == 'f') {
+			new_str[0] = '\f';
+			i += 2;
+		}
+		else if (str[1] == 't') {
+			new_str[0] = '\t';
+			i += 2;
 		}
 	}
+	return (i);
 }
 
 char	*m_format_param(char *str, int max)
@@ -41,27 +81,48 @@ char	*m_format_param(char *str, int max)
 	int 	i;
 	int 	j;
 	char 	*new_str;
+	int 	mode;
+	int 	k;
 
-	printf("%d %s\n", max, str);
+	printf("%s\n", str);
 
 	i = 0;
 	j = 0;
 	new_str = ft_strnew(max);
+	mode = 0;
+	if (str[0] == '"')
+		mode = 1;
+	if (str[0] == '\'')
+		mode = 2;
 	while(str[i] && i < max)
 	{
-		printf("a [%c] %d\n", str[i], i);
-		if (str[i] == '\\') {
-			m_set_escaped_character(&new_str[j], &str[i], &i, max);
-			j++;
+		//printf("a [%c] %d\n", str[i], i);
+		if (str[i] == '\\' && (i + 1) < max) {
+			if (mode == 0 && str[i + 1] != '\\')
+				i++;
+			else {
+				k = m_set_escaped_character(&new_str[j], &str[i], mode);
+				printf("%d\n", i);
+				if (k > 0)
+					j++;
+				i += k;
+			}
 		}
-		else {
+		if (
+			(str[i] == '"' && mode != 1) ||
+			(str[i] == '\'' && mode != 2) ||
+			mode == 0 ||
+			(str[i] != '"' && str[i] != '\''))
+		{
 			new_str[j] = str[i];
 			j++;
 		}
-		printf("b [%c] %d\n", str[i], i);
+		//printf("b [%c] %d\n", str[i], i);
 		i++;
 	}
 	new_str[j] = '\0';
+	if (ft_strlen(new_str) == 0)
+		ft_strdel(&new_str);
 	return (new_str);
 }
 
