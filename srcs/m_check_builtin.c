6ee58ee8b640e6a52f7446a0b32dbd96fda17d5a
Hadouweb <kengo14@hotmail.fr>
2016-11-05 06:58:11 +0100

    Norm

--- srcs/m_check_builtin.c
+++ srcs/m_check_builtin.c
@@ -1,13 +1,55 @@
 #include "minishell.h"
 
+char 	*m_get_builtin(char *cmd)
+{
+	int 	i;
+	int 	j;
+	int 	token;
+	int 	token2;
+	char 	*builtin;
+
+	i = 0;
+	j = 0;
+	token = 0;
+	token2 = 0;
+	builtin = ft_strnew(ft_strlen(cmd));
+	while (cmd[i])
+	{
+		if (token == 2 || token2 == 2)
+			;
+		if (cmd[i] == '"')
+			token++;
+		else if (cmd[i] == '\'')
+			token2++;
+		else {
+			builtin[j] = cmd[i];
+			j++;
+		}
+		i++;
+	}
+	builtin[j] = '\0';
+	if (builtin == NULL || ft_strlen(builtin) == 0)
+		return (NULL);
+	return (builtin);
+}
+
 int		m_check_builtin(t_app *app, char *cmd)
 {
 	int		ret;
+	char 	*builtin;
 
 	ret = 0;
-	ft_strdel(&cmd);
-	ft_lstprint(app->param, NULL);
-	cmd = (char*)app->param->content;
+
+	builtin = m_get_builtin(cmd);
+
+	int i = 0;
+	while (builtin[i])
+	{
+		printf("|%c| ", builtin[i]);
+		i++;
+	}
+	printf("\n");
+	printf("[%s] %d\n", builtin, ft_strcmp(cmd, "exit"));
 	if (ft_strcmp(cmd, "exit") == 0 && (ret = 1))
 	{
 		m_free_all(app);
